b18403d26160df443d7f6495836af520b0af7df5
Henrique Recidive <recidive@gmail.com>
2017-04-21 21:38:55 -0300

    Moved types from classes to simple objects.

--- src/types/scalars.js
+++ src/types/scalars.js
@@ -1,25 +1,17 @@
-class BaseScalarType {
-  constructor() {
-    if (typeof this.name !== 'string') {
-      throw new Error(`A scalar type needs a name.`);
-    }
-  }
+export const string = {
+  name: 'string',
+  parse: value => String(value),
+  serialize: value => String(value)
 }
 
-class StringType extends BaseScalarType {
-
-  // name: 'string'
-
-  parse(value) {
-    return String(value)
-  }
-
-  serialize(value) {
-    return String(value)
-  }
-
+export const number = {
+  name: 'number',
+  parse: value => Number(value),
+  serialize: value => Number(value)
 }
 
-export default {
-  StringType: new StringType()
+export const boolean = {
+  name: 'boolean',
+  parse: value => Boolean(value),
+  serialize: value => Boolean(value)
 }
