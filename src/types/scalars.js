018d56bb472004bb010e3560c108adb68df2ab85
Henrique Recidive <recidive@gmail.com>
2017-04-23 13:50:43 -0300

    Added constants for scalar type names.

--- src/types/scalars.js
+++ src/types/scalars.js
@@ -1,17 +1,21 @@
+export const STRING = 'string'
+export const NUMBER = 'number'
+export const BOOLEAN = 'boolean'
+
 export const string = {
-  name: 'string',
+  name: STRING,
   parse: value => String(value),
   serialize: value => String(value)
 }
 
 export const number = {
-  name: 'number',
+  name: NUMBER,
   parse: value => Number(value),
   serialize: value => Number(value)
 }
 
 export const boolean = {
-  name: 'boolean',
+  name: BOOLEAN,
   parse: value => Boolean(value),
   serialize: value => Boolean(value)
 }
